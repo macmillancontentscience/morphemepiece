c8946654ce9b2a7889d679b09a1d2ac3d345966c
Jon Harmon <jonthegeek@gmail.com>
2021-08-27 16:11:44 -0500

    data (#7)
    * Use dlr and data package.
    
    Also reorganized files to logically separate things a bit more.
    
    * Clean vignette.
    
    Move toward exportable functions (although we aren't quite there yet).
    
    * Goodpractice updates.
    
    * Reference *piece algorithms without braces.
    
    * Fix upstream reference.
    
    * Actually fix upstream reference.
    
    * Updated/simplified with improved dlr.

--- vignettes/make_vocab_and_lookup.R
+++ vignettes/make_vocab_and_lookup.R
@@ -3,26 +3,38 @@
 # Optionally, a word frequency table from some corpus can be given.
 #
 
-make_vocab_and_lookup <- function(processed_wiktionary,
-                                  wp_vocab,
-                                  target_vocab_size = 25000,
-                                  word_freq_tab = NULL) {
-  unnested_processed_wiktionary <- unnest_pw(processed_wiktionary, clean = TRUE)
-  token_counts <- get_token_counts(unnested_processed_wiktionary, word_freq_tab)
+# Throughout this code, I've followed the "begin internal functions with '.'"
+# convention to indicate thoughts about eventual internal/exported functions.
+# For now these will remain just in the vignette, but we should slowly migrate
+# them into the package (with refinement wherever we made some very specific
+# opinionated decisions).
+
+make_vocab_and_lookup <- function(full_lookup,
+                                  full_vocabulary = wikimorphemes::wiktionary_word_list(),
+                                  wordpiece_vocab = wordpiece.data::wordpiece_vocab(),
+                                  target_vocab_size = 30000L,
+                                  word_frequency_table = NULL) {
+  full_lookup <- .add_words_to_lookup(full_lookup, full_vocabulary)
+
+  unnested_lookup <- .unnest_lookup(full_lookup, clean = TRUE)
+  token_counts <- count_tokens(
+    unnested_lookup,
+    word_frequency_table
+  )
 
   # punctuation, numbers, [CLS], etc. from wordpiece vocab
-  fancy_wp_tokens <- get_fancy_wp_tokens(wp_vocab)
+  fancy_wp_tokens <- .get_fancy_wp_tokens(wordpiece_vocab)
 
-  actual_wp_words <- get_actual_wp_words(processed_wiktionary, wp_vocab)
+  actual_wp_words <- .get_actual_wp_words(full_lookup, wordpiece_vocab)
   # short actual words from wp will get a VIP pass directly to vocab.
   # Though most of them wouldn't be considered words by most people. :)
   short_words <- actual_wp_words[nchar(actual_wp_words) <= 3]
   rest_of_words <- actual_wp_words[nchar(actual_wp_words) > 3]
   # make sure we keep enough tokens to cover the rest of the wp words
-  processed_tokens <- unnested_processed_wiktionary %>%
-    dplyr::filter(word %in% rest_of_words) %>%
-    dplyr::distinct(token) %>%
-    dplyr::pull(token)
+  processed_tokens <- unnested_lookup %>%
+    dplyr::filter(.data$word %in% rest_of_words) %>%
+    dplyr::distinct(.data$token) %>%
+    dplyr::pull(.data$token)
 
   # we *didn't* clean the processed tokens before this, so clean them now.
   # This mainly filters out a few capitalized proper nouns that sneaked back in,
@@ -31,7 +43,7 @@ make_vocab_and_lookup <- function(processed_wiktionary,
                                                            pattern = "[^a-z#]",
                                                            negate = TRUE)]
 
-  wp_proper_nouns <- get_wp_proper_nouns(processed_wiktionary, wp_vocab)
+  wp_proper_nouns <- .get_wp_proper_nouns(full_lookup, wordpiece_vocab)
   # There's a lot of overlap with "actual" words (e.g. apple). This
   # *shouldn't* make a difference, but just to be sure, filter those out:
   wp_proper_nouns <- setdiff(wp_proper_nouns, actual_wp_words)
@@ -62,63 +74,120 @@ make_vocab_and_lookup <- function(processed_wiktionary,
     # Maybe later be more sophisticated about proper nouns.
     n_tokens_to_add <- target_vocab_size - length(vocab)
     additional_tokens <- token_counts %>%
-      dplyr::filter(! token %in% vocab) %>%
-      dplyr::arrange(rank) %>%
+      dplyr::filter(!(.data$token %in% vocab)) %>%
+      dplyr::arrange(.data$rank) %>%
       utils::head(n_tokens_to_add) %>%
-      dplyr::pull(token)
+      dplyr::pull(.data$token)
     vocab <- unique(c(vocab, additional_tokens))
   }
-  lookup <- make_lookup(unnested_processed_wiktionary, vocab)
+  lookup <- .make_lookup(unnested_lookup, vocab)
   return(list(vocab = vocab, lookup = lookup))
 }
 
-unnest_pw <- function(processed_wiktionary, clean = TRUE) {
+.add_words_to_lookup <- function(full_lookup, full_vocabulary) {
+  missing_words <- dplyr::tibble(word = full_vocabulary) %>%
+    dplyr::anti_join(
+      dplyr::distinct(full_lookup, .data$word),
+      by = "word"
+    ) %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(
+      morphemes = list(c(base_word = .data$word)),
+      n_morphemes = 1L
+    ) %>%
+    dplyr::ungroup()
+
+  return(
+    dplyr::distinct(
+      dplyr::bind_rows(
+        full_lookup,
+        missing_words
+      )
+    )
+  )
+}
+
+.unnest_lookup <- function(lookup, clean = TRUE) {
   if (clean) { # keep only words in all lowercase latin alphabet
-    processed_wiktionary <- processed_wiktionary %>%
-      dplyr::filter(!stringr::str_detect(word, "[^a-z]"))
+    lookup <- dplyr::filter(
+      lookup,
+      !stringr::str_detect(.data$word, "[^a-z]")
+    )
   }
-  upw <- processed_wiktionary %>%
-    tidyr::unnest_longer(morphemes) %>%
-    dplyr::filter(morphemes != "") %>%
-    dplyr::mutate(before = ifelse(morphemes_id %in% c("suffix",
-                                                      "inflection",
-                                                      "interfix"),
-                                  "##", ""),
-                  after = ifelse(morphemes_id == "prefix", "##", ""),
-                  token = paste0(before, morphemes, after)) %>%
-    dplyr::rename(morpheme_type = morphemes_id) %>%
-    dplyr::select(word, token, morpheme_type)
+  if (!nrow(lookup)) {
+    return(
+      dplyr::tibble(
+        word = character(0),
+        token = character(0),
+        morpheme_type = character(0)
+      )
+    )
+  }
+
+  unnested_lookup <- tidyr::unnest_longer(
+    data = lookup,
+    col = .data$morphemes,
+    indices_to = "morpheme_type"
+  ) %>%
+    dplyr::filter(.data$morphemes != "") %>%
+    dplyr::mutate(
+      before = ifelse(
+        test = .data$morpheme_type %in% c("suffix", "inflection", "interfix"),
+        yes = "##",
+        no = ""
+      ),
+      after = ifelse(
+        test = .data$morpheme_type == "prefix",
+        yes = "##",
+        no = ""
+      ),
+      morphemes = stringr::str_remove_all(.data$morphemes, "-"),
+      token = paste0(.data$before, .data$morphemes, .data$after)
+    ) %>%
+    dplyr::select(.data$word, .data$token, .data$morpheme_type)
 
   if (clean) {
     # Don't remove tokens here! Otherwise we'll get incomplete words.
     # BUT... let's cast all tokens to lowercase at this point.
-    upw <- upw %>%
-      dplyr::mutate(token = tolower(token))
+    unnested_lookup <- dplyr::mutate(
+      unnested_lookup,
+      token = tolower(.data$token)
+    )
   }
 
-  return(upw)
+  return(unnested_lookup)
 }
-unnest_pw <- memoise::memoise(unnest_pw)
 
-get_token_counts <- function(unnested_processed_wiktionary,
-                             word_freq_tab = NULL) {
-  # if word_freq_tab is supplied, weight by word freq
-  if (!is.null(word_freq_tab)) {
-    unnested_processed_wiktionary <- dplyr::left_join(
-      unnested_processed_wiktionary,
-      word_freq_tab,
-      by = c("word" = "word")) %>%
-      dplyr::mutate(word_count = ifelse(is.na(word_count), 1, word_count))
+.unnest_lookup <- memoise::memoise(.unnest_lookup)
+
+# Eventually we'll want to clean this one up and export it, I think.
+count_tokens <- function(unnested_lookup,
+                         word_frequency_table = NULL) {
+  # if word_frequency_table is supplied, weight by word freq
+  if (!is.null(word_frequency_table)) {
+    unnested_lookup <- dplyr::left_join(
+      unnested_lookup,
+      word_frequency_table,
+      by = "word"
+    ) %>%
+      dplyr::mutate(
+        word_count = as.integer(.data$word_count),
+        word_count = ifelse(
+          test = is.na(.data$word_count),
+          1L,
+          .data$word_count
+        )
+      )
   } else {
-    unnested_processed_wiktionary <- unnested_processed_wiktionary %>%
-      dplyr::mutate(word_count = 1)
+    unnested_lookup <- unnested_lookup %>%
+      dplyr::mutate(word_count = 1L)
   }
-  token_counts <- unnested_processed_wiktionary %>%
-    dplyr::group_by(token) %>%
-    dplyr::summarize(token_score = sum(word_count)) %>%
-    dplyr::arrange(dplyr::desc(token_score)) %>%
+  token_counts <- unnested_lookup %>%
+    dplyr::group_by(.data$token) %>%
+    dplyr::summarize(token_score = sum(.data$word_count)) %>%
+    dplyr::arrange(dplyr::desc(.data$token_score)) %>%
     dplyr::mutate(rank = dplyr::row_number()) %>%
-    dplyr::filter(!stringr::str_detect(token, "[^a-z#]"))
+    dplyr::filter(!stringr::str_detect(.data$token, "[^a-z#]"))
 
   return(token_counts)
 }
@@ -126,72 +195,78 @@ get_token_counts <- function(unnested_processed_wiktionary,
 # token_counts <- get_token_counts(unnested_processed_wiktionary)
 # token_counts <- get_token_counts(unnested_processed_wiktionary, word_freq_tab)
 
-get_fancy_wp_tokens <- function(wp_vocab) {
+.get_fancy_wp_tokens <- function(wp_vocab) {
   # exclude [unused]
   is_fancy <- stringr::str_detect(names(wp_vocab), pattern = "[^a-z#]")
   is_unused <- stringr::str_detect(names(wp_vocab), pattern = "\\[unused")
   # note that this excludes tokens "#", "##"... add those back in
-  return(c(names(wp_vocab[is_fancy&!is_unused]), "#", "##"))
+  return(c(names(wp_vocab[is_fancy & !is_unused]), "#", "##"))
 }
 
-get_actual_wp_words <- function(processed_wiktionary, wp_vocab) {
+.get_actual_wp_words <- function(full_lookup, wp_vocab) {
   # wordpieces that have a wiktionary entry
-  clean_wik <- processed_wiktionary %>%
-    dplyr::filter(!stringr::str_detect(word, "[^a-z]")) %>%
-    dplyr::pull(word)
+  clean_wik <- full_lookup %>%
+    dplyr::filter(!stringr::str_detect(.data$word, "[^a-z]")) %>%
+    dplyr::pull(.data$word)
   in_wik <- names(wp_vocab) %in% clean_wik
   return(names(wp_vocab[in_wik]))
 }
 
-
-get_wp_proper_nouns <- function(processed_wiktionary, wp_vocab) {
+.get_wp_proper_nouns <- function(full_lookup, wp_vocab) {
   # Proper nouns are capitalized in Wiktionary. This gets a *lot* of words,
   # but most of the weird ones won't be in WP vocab anyway.
-  cap_words <-  processed_wiktionary %>%
+  cap_words <-  full_lookup %>%
     # still exclude words with non-alpha chars:
-    dplyr::filter(!stringr::str_detect(word, "[^[:alpha:]]"),
-           stringr::str_starts(word, "[A-Z][a-z]+")) %>%
-    dplyr::pull(word) %>%
+    dplyr::filter(
+      !stringr::str_detect(.data$word, "[^[:alpha:]]"),
+      stringr::str_starts(.data$word, "[A-Z][a-z]+")
+    ) %>%
+    dplyr::pull(.data$word) %>%
     tolower()
 
   in_wik <- names(wp_vocab) %in% cap_words
   return(names(wp_vocab[in_wik]))
 }
 
-make_lookup <- function(unnested_processed_wiktionary, vocab) {
-  covered_words <- unnested_processed_wiktionary %>%
-    dplyr::mutate(token_in_vocab = token %in% vocab) %>%
-    dplyr::group_by(word) %>%
-    dplyr::summarize(covered = all(token_in_vocab)) %>%
-    dplyr::filter(covered) %>%
-    dplyr::select(word)
+.make_lookup <- function(unnested_lookup, vocab) {
+  covered_words <- unnested_lookup %>%
+    dplyr::mutate(token_in_vocab = .data$token %in% vocab) %>%
+    dplyr::group_by(.data$word) %>%
+    dplyr::summarize(covered = all(.data$token_in_vocab)) %>%
+    dplyr::filter(.data$covered) %>%
+    dplyr::select(.data$word)
 
-  lookup_df <- dplyr::semi_join(unnested_processed_wiktionary, covered_words,
-                   by = c("word" = "word")) %>%
-    dplyr::group_by(word) %>%
-    dplyr::summarize(tokenization = paste(token, collapse = " ")) %>%
+  lookup_df <- dplyr::semi_join(
+    unnested_lookup, covered_words,
+    by = "word"
+  ) %>%
+    dplyr::group_by(.data$word) %>%
+    dplyr::summarize(tokenization = paste(.data$token, collapse = " ")) %>%
     # insert the ## between base words
-    dplyr::mutate(tokenization = stringr::str_replace_all(tokenization,
-                                                          "([a-z]) ([a-z])",
-                                                          "\\1 ## \\2"))
+    dplyr::mutate(
+      tokenization = stringr::str_replace_all(
+        .data$tokenization,
+        "([a-z]) ([a-z])",
+        "\\1 ## \\2")
+    )
   return(lookup_df)
 }
 
 # we save the lookup as text in a particular (simple) format, for general
 # distribution. We can make future performance faster by putting common words
 # near the top.
-make_text_lookup <- function(voc, lu, word_freq_tab = NULL) {
+.make_text_lookup <- function(voc, lu, word_freq_tab = NULL) {
   # first let's take words with no breakdown out of the lookup list (they are
   # already in the vocab, along with other no-breakdown words that aren't in
   # lookup, such as proper nouns).
   lu <- lu %>%
-    dplyr::filter(!word %in% voc)
+    dplyr::filter(!(.data$word %in% voc))
 
   if (!is.null(word_freq_tab)) {
     # arrange lookup so that common words are first.
     lu <- lu %>%
       dplyr::left_join(word_freq_tab, by = "word") %>%
-      dplyr::arrange(dplyr::desc(word_count))
+      dplyr::arrange(dplyr::desc(.data$word_count))
   }
 
   # the text lookup is basically the first two columns...
